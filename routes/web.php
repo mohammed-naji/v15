274e35d85142b7925ef10f5c3aab481fcd3eae70
Mohammed Naji <m.naji1512@gmail.com>
2023-06-01 18:22:43 +0300

    routs

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 // => // array key => value
 // :: // class :: static method
 
+// method chain
+
 // use , namespace
 
 // Route::get('url', 'Action');
@@ -27,28 +29,116 @@ use Illuminate\Support\Facades\Route;
 //     return 'Homepage - Get';
 // });
 
+// Route::get('/', function() {
+//     return 'Home';
+// });
+
+// Route::get('about', function() {
+//     return 'About Page';
+// });
+
+// Route::get('contact', function() {
+//     return 'Contact Page';
+// });
+
+// include 'admin.php';
+
+// Route::match(['put', 'patch'], 'edit-profile', function() {
+//     return 'Edit Profile';
+// });
+
+// Route::any('/policy', function() {
+//     return 'Policy Page';
+// });
+
+// Route::get('post/{id}', function($id) {
+//     return "Post ID : " . $id;
+// })->whereNumber('id');
+
+// Route::get('profile/{name}/{age}/{user}', function($name, $age, $user) {
+//     return "Welcome $name, your age is $age, Username is $user";
+// })->where('name', '[A-Za-z]+')->whereNumber('age')->whereAlphaNumeric('user');
+
+
+// https://bakkah.com/sessions/capm
+// https://bakkah.com/sessions/capm/online-training
+
+// Route::get('sessions/{course}/{type?}', function($course, $type = '') {
+
+//     if(empty($type)) {
+//         return "Course : $course";
+//     }else {
+//         return "Course : $course , Type : $type";
+//     }
+// });
+
 Route::get('/', function() {
     return 'Home';
 });
 
-Route::get('about', function() {
-    return 'About Page';
+Route::get('/about', function() {
+    return 'About';
 });
 
-Route::get('contact', function() {
-    return 'Contact Page';
+Route::get('/contact', function() {
+    return 'Contact page';
 });
 
-// include 'admin.php';
-
-Route::match(['put', 'patch'], 'edit-profile', function() {
-    return 'Edit Profile';
+Route::post('/contact', function() {
+    return 'Contact post';
 });
 
-Route::any('/policy', function() {
-    return 'Policy Page';
+Route::get('/posts/{id?}', function($id = '') {
+    return 'Posts';
 });
 
-Route::get('post/{id}', function($id) {
-    return "Post ID : " . $id;
+Route::post('subscribe', function() {
+    return 'Subscribe';
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('our-team', function() {
+    return 'Our Team';
+})->name('teampage');
+
+// DRY => DON'T REPEAT YOURSELF
+
+// $page = 'admin';
+
+// Route::get($page.'/posts', function() {});
+// Route::get($page.'/comment', function() {});
+// Route::get($page.'/products', function() {});
+// Route::get($page.'/orders', function() {});
+// Route::get($page.'/users', function() {});
+// Route::get($page.'/roles', function() {});
+
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/posts', function() {})->name('posts');
+    Route::get('/comment', function() {})->name('comment');
+    Route::get('/products', function() {})->name('products');
+    Route::get('/orders', function() {})->name('orders');
+    Route::get('/users', function() {})->name('users');
+    Route::get('/roles', function() {})->name('roles');
+});
+
+
+
+
+
+
+
+
+
